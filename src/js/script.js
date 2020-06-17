c985e076c8c4225c6e92f7b42e491007daaeca67
VladProfitkit <vlad@profitkit.ru>
2020-06-17 19:10:10 +0300

    Футер с адаптивом, шапка без адаптива и рабочего слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
-  
+  //главное меню:
+  const mainSubMenu = $('.header__main-submenu');
+  const submenuLinks = $('.header__main-submenu-link')
+
+  mainSubMenu.each(function () {
+    $(this).mouseenter(function () {
+      $(this).siblings('.header__main-menu-link--parent').addClass('active');
+      if ($(this).parent().hasClass('header__main-submenu-item--parent')) {
+        $(this).siblings('.header__main-submenu-link--parent').addClass('active');
+      };
+    });
+
+    $(this).mouseleave(function () {
+      $(this).siblings('.header__main-menu-link--parent').removeClass('active');
+      if ($(this).parent().hasClass('header__main-submenu-item--parent')) {
+        $(this).siblings('.header__main-submenu-link--parent').removeClass('active');
+      };
+    });
+  });
+
+  submenuLinks.each(function () {
+    if ($(this).parent().parent().parent().hasClass('header__main-submenu-item--parent')) {
+      $(this).focus(function () {
+        $(this).parent().parent().parent().children('.header__main-submenu-link--parent').addClass('active');
+      });
+
+      $(this).blur(function () {
+        $(this).parent().parent().parent().children('.header__main-submenu-link--parent').removeClass('active');
+      });
+    };
+  });
+
+
+  //слайдер в шапке:
+  const mainSlider = $('.');
 });
